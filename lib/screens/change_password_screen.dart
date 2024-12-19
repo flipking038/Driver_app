62403fc34ca66df31f3ba22cc4265345676cfa64
Phillip Fawzy <flipking037@gmail.com>
2024-12-19 14:37:31 +0200

    Create  ChangePasswordScreen & FormChangePassword (With Log in Cubit (now))

--- lib/screens/change_password_screen.dart
+++ lib/screens/change_password_screen.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:telgani/constant.dart';
+import 'package:telgani/widgets/change_password_widgets/form_change_password.dart';
 
 class ChangePasswordScreen extends StatelessWidget {
   const ChangePasswordScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kPrimaryColor,
+        title: const Text('Change Password'),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 18),
+      ),
+      body: const Padding(
+          padding: EdgeInsets.only(left: 24, right: 16),
+          child: FormChangePassword()),
+    );
   }
 }
---
+++ lib/widgets/change_password_widgets/form_change_password.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:telgani/constant.dart';
+import 'package:telgani/cubit/login/login_cubit.dart';
+import 'package:telgani/widgets/login_widgets/login_button.dart';
+import 'package:telgani/widgets/text_field_widget.dart';
+
+class FormChangePassword extends StatelessWidget {
+  const FormChangePassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: BlocProvider.of<LoginCubit>(context).loginKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          const SizedBox(height: 58),
+          TextFieldWidget(
+            phoneNumber: false,
+            controller: BlocProvider.of<LoginCubit>(context).loginPassword,
+            label: 'Password',
+            hintText: 'Password',
+            onChanged: (value) =>
+                BlocProvider.of<LoginCubit>(context).password = value,
+            obscurePassword:
+                BlocProvider.of<LoginCubit>(context).obscurePassword,
+            visibilityIcon: IconButton(
+              onPressed: () =>
+                  BlocProvider.of<LoginCubit>(context).showAndHidePassword(),
+              icon: BlocProvider.of<LoginCubit>(context).obscurePassword == true
+                  ? const Icon(
+                      Icons.visibility,
+                      color: kPrimaryColor,
+                    )
+                  : const Icon(Icons.visibility_off),
+            ),
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            phoneNumber: false,
+            controller: BlocProvider.of<LoginCubit>(context).loginPassword,
+            label: 'New Password',
+            hintText: 'New Password',
+            onChanged: (value) =>
+                BlocProvider.of<LoginCubit>(context).password = value,
+            obscurePassword:
+                BlocProvider.of<LoginCubit>(context).obscurePassword,
+            visibilityIcon: IconButton(
+              onPressed: () =>
+                  BlocProvider.of<LoginCubit>(context).showAndHidePassword(),
+              icon: BlocProvider.of<LoginCubit>(context).obscurePassword == true
+                  ? const Icon(
+                      Icons.visibility,
+                      color: kPrimaryColor,
+                    )
+                  : const Icon(Icons.visibility_off),
+            ),
+          ),
+          const SizedBox(height: 24),
+          TextFieldWidget(
+            phoneNumber: false,
+            controller: BlocProvider.of<LoginCubit>(context).loginPassword,
+            label: 'Confirm Password',
+            hintText: 'Confirm Password',
+            onChanged: (value) =>
+                BlocProvider.of<LoginCubit>(context).password = value,
+            obscurePassword:
+                BlocProvider.of<LoginCubit>(context).obscurePassword,
+            visibilityIcon: IconButton(
+              onPressed: () =>
+                  BlocProvider.of<LoginCubit>(context).showAndHidePassword(),
+              icon: BlocProvider.of<LoginCubit>(context).obscurePassword == true
+                  ? const Icon(
+                      Icons.visibility,
+                      color: kPrimaryColor,
+                    )
+                  : const Icon(Icons.visibility_off),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            'Forgot Password ?',
+            style: TextStyle(
+                color: kPrimaryColor,
+                fontWeight: FontWeight.w900,
+                fontSize: 14),
+          ),
+          const SizedBox(height: 32),
+          LoginButtton(
+            onPressed: () {
+              if (BlocProvider.of<LoginCubit>(context)
+                  .loginKey
+                  .currentState!
+                  .validate()) {
+                BlocProvider.of<LoginCubit>(context).login();
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
